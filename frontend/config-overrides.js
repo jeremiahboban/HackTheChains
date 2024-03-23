de24ead5923bde446cd689f1b03aeae96c3c464b
jbthelegend <jbthelegend15@gmail.com>
2024-03-23 10:58:06 -0500

    changed react-scripts to react-app-rewried

---
+++ frontend/config-overrides.js
@@ -0,0 +1,16 @@
+// config-overrides.js
+
+const { override, addWebpackResolve } = require('customize-cra');
+
+module.exports = override(
+  addWebpackResolve({
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      url: require.resolve('url/')
+    }
+  })
+);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, ReactNode, useMemo } from 'react';
 
 function App() {
   return (
