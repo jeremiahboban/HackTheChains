59a674b9b61b480e54613b770c8cc344adb55f85
jbthelegend <jbthelegend15@gmail.com>
2024-03-23 11:42:22 -0500

    set up phantom wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home";
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import React, { FC, ReactNode, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+import React, {FC, ReactNode, useMemo} from 'react';
+import './App.css';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
   return (
@@ -16,8 +20,45 @@ function App() {
               <Route path="/" element={<Home />} />
           </Routes>
       </BrowserRouter>
+      <div>
+       <Context>
+       <Content />
+     </Context>
+     </div>
     </div>
-  );
+    
+   );
+ }
+ 
+ export default App;
+ 
+ const Context = ({ children }) => {
+  // const network = WalletAdapterNetwork.Devnet;
+  // const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const endpoint = "http://localhost:8899"; // local cluster override
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter()
+    ],
+    []
+  )
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
 
-export default App;
+const Content = () => {
+  return (
+    <div className="App">
+      <WalletMultiButton />
+    </div>
+  )
+}
