f9d27184600fc1c2ac34ebe98f8ad80d15166d37
Veer <veermshah@users.noreply.github.com>
2024-03-23 21:02:23 -0500

    donations page update

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,13 +6,19 @@ export default function Navbar(props) {
         <>
             <header className="bg-blue-500 p-4">
                 <nav className="flex justify-left space-x-4">
+                    <Link to="/">
+                        <img
+                            src={"logo.png"}
+                            alt="Charity Clarity Logo"
+                            className="h-12"
+                        />
+                    </Link>
                     <Link
                         to="/"
                         className={`text-white px-4 py-2 rounded-lg font-bold text-xl `}
                     >
                         Charity Clarity
                     </Link>
-            
                     <Link
                         to="/donate"
                         className={`${
@@ -33,9 +39,28 @@ export default function Navbar(props) {
                     >
                         Contributions
                     </Link>
-            
+                    <Link
+                        to="/contributions"
+                        className={` ${
+                            props.active === "contributions"
+                                ? "bg-white text-blue-500"
+                                : "text-white"
+                        } px-4 py-2 rounded-full font-bold text-xl hover:bg-white hover:text-blue-500`}
+                    >
+                        Contributions
+                    </Link>
+                    <div style={{ width: "500px" }}></div> {/* Spacer */}
+                    <Link
+                        to="/login"
+                        className={`justify-right ${
+                            props.active === "login"
+                                ? "bg-white text-blue-500"
+                                : "text-white"
+                        } px-4 py-2 rounded-full font-bold text-xl hover:bg-white hover:text-blue-500`}
+                    >
+                        Login
+                    </Link>
                 </nav>
-                
             </header>
         </>
     );
